70f9fad6838271e390e2fe7de438742f33cdebb5
yuika nagata <xxx.yu.74@gmail.com>
2022-12-09 15:06:31 +0900

    セットアップ

---
+++ src/main/java/com/raisetech/pokemonbox/PokemonboxApplication.java
@@ -0,0 +1,13 @@
+package com.raisetech.pokemonbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raisetech/pokemonbox/PokemonboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raisetech.pokemonbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
